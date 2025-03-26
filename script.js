634f98b86626149a95574b401d9f70b694a96ea5
KasparasBal <Kasparasbalys@gmail.com>
2025-03-26 17:39:57 +0200

    added table html & adding to page logic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const tableBody = document.getElementById("table-body");
+
 const myLibrary = [];
 
 function Book(title, year, author) {
@@ -13,7 +15,28 @@ function addBookToLibrary(title, year, author) {
   console.log(myLibrary);
 }
 
+function addBooksToCanvas(bookArray) {
+  bookArray.forEach((book) => {
+    const tableRow = document.createElement("tr");
+    const titleCell = document.createElement("td");
+    const authorCell = document.createElement("td");
+    const yearCell = document.createElement("td");
+
+    titleCell.textContent = book.title;
+    authorCell.textContent = book.author;
+    yearCell.textContent = book.year;
+
+    tableRow.appendChild(titleCell);
+    tableRow.appendChild(authorCell);
+    tableRow.appendChild(yearCell);
+
+    tableBody.appendChild(tableRow);
+  });
+}
+
 addBookToLibrary("Harry Potter and the Sorcerer's Stone", 1997, "J.K. Rowling");
 addBookToLibrary("The Lord of the Rings", 1954, "J.R.R. Tolkien");
 addBookToLibrary("Pride and Prejudice", 1813, "Jane Austen");
 addBookToLibrary("Dune", 1965, "Frank Herbert");
+
+addBooksToCanvas(myLibrary);
